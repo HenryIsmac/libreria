e788e25ff89bdc9a849eb97ae652ece0892e9144
Henry Andrade <Henry Andrade@DESKTOP-QD4LO5B>
2024-11-27 23:08:25 -0500

    Conexión a MYSQL

--- src/com/distribuida/principal/AutorPrincipal.java
+++ src/com/distribuida/principal/AutorPrincipal.java
@@ -6,7 +6,7 @@ public class AutorPrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Autor autor = new Autor(1, "nombre", "apellido", "ecuador", "direccion", "9999999999", "correo");
+		Autor autor = new Autor(1, "Henry", "Andrade", "Quito", "CDP", "1721029191", "henry@gmail.com");
 		System.out.println(autor.toString());
 	}
 
